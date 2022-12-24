3f1e5746c7289ccd57843d442c30fc43f065fa25
arctia <arctia.games@gmx.com>
2022-12-24 07:58:20 +0100

    update chars

--- update_characters.sh
+++ update_characters.sh
@@ -1,7 +1,7 @@
 echo "uploading Images..."
 py UploadImages.py
 echo "uploading Character/s..."
-py Character.py -u y -d j -i 199,200,202,20063,203 -c r
+py Character.py -u y -d j -i 205,206,20012 -c r
 echo "uploading Secondary Tables..."
 py CharaTable.py -u y -d j
 py CharaSymbol.py -u y -d j
