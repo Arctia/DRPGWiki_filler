dfd4f228902b010e77a5c09704dcd3e49b289035
arctia <arctia.games@gmx.com>
2023-06-14 16:01:42 +0200

    Add leaderskills to patcher

--- patcher.py
+++ patcher.py
@@ -3,8 +3,9 @@ import UnityPy, platform, shutil, json
 from gst import *
 
 src = "C:\\Users\\Arctia\\AppData\\LocalLow\\disgaearpg\\DisgaeaRPG\\assetbundle\\masters2\\"
-if platform.system == 'Linux': src = os.path.join("datas", "masters")
+if platform.system == 'Linux': src = os.path.join("datas", "JPMasters")
 
+src = os.path.join("datas", "JPMasters")
 env = UnityPy.load(src)
 path_M = os.path.join("datas", "JPMasters")
 
@@ -59,7 +60,7 @@ def	replace_values(chara:dict, wc, col:int) -> None:
 worksheets = ['Unique Human', 'Unique Human 2', 'Unique Monster',
 				'Human Generic', 'Monster Generic']
 
-def patcher(characters:dict, skills:dict) -> None:
+def patcher(characters:dict, skills:dict, leaderskills:dict) -> None:
 	for ws in worksheets:
 		ws = workbook[ws]
 		wc = ws.cell
@@ -89,9 +90,44 @@ def patcher(characters:dict, skills:dict) -> None:
 						val:str = wc(RowIDS[f"Skill Effect {i}"] + 1, col + 1).value
 						if val != None: s['description_effect'] = val
 
-			print(f"[DONE  ]: character <{c['id']}>")
-			
 			# replace all leaderskills
+			ll = [c['m_leader_skill_id'], c['m_leader_skill_id_sub_1'], c['m_leader_skill_id_sub_2'], c['m_leader_skill_id_sub_3']]
+			al = [c['additional_m_leader_skill_id'], c['additional_m_leader_skill_id_sub_1'], c['additional_m_leader_skill_id_sub_2'], c['additional_m_leader_skill_id_sub_3']]
+			for i in range(0, 4):
+				lid:int = ll[i]
+				alid:int = al[i]
+				for l in leaderskills:
+					if l['id'] != lid: continue
+
+					name:str = wc(RowIDS[f"Evility Name {i + 1}"] + 1, col + 1).value
+					if name != None: l['name'] = name
+					effect:str = wc(RowIDS[f"Evility Desc {i + 1}"] + 1, col + 1).value
+					if effect == None: continue
+
+					# split if double leaderskill
+					if alid != 0:
+						if "," in effect:
+							splitted = effect.rsplit(",", 1)
+						elif "#PER2#" in effect:
+							splitted = effect.rsplit("#PER2#", 1)
+							splitted[1] = f"#PER2#{splitted[1]}"
+						elif "and" in effect:
+							splitted = effect.rsplit("and", 1)
+						elif "&" in effect:
+							splitted = effect.rsplit("&", 1)
+						else:
+							continue
+
+						l['description'] = splitted[0]
+						for l in leaderskills:
+							if l['id'] != lid: continue
+							l['description'] = splitted[1].replace("PER2", "PER")
+							break
+					else:
+						l['description'] = effect
+					break
+
+			print(f"[DONE  ]: character <{c['id']}>")
 
 def story_patcher(dialogues) -> None:
 
@@ -115,19 +151,22 @@ def story_patcher(dialogues) -> None:
 					break
 
 def main():
-	# obj_char 	= load_unitypy_obj("character")
-	# characters 	= obj_char.read_typetree()
-	# obj_skills 	= load_unitypy_obj("command")
-	# skills 		= obj_skills.read_typetree()
+	obj_char 	= load_unitypy_obj("character")
+	characters 	= obj_char.read_typetree()
+	obj_skills 	= load_unitypy_obj("command")
+	skills 		= obj_skills.read_typetree()
+	obj_lskills 	= load_unitypy_obj("leaderskill")
+	lskills 		= obj_lskills.read_typetree()
 	obj_stories	= load_unitypy_obj("storytalk")
 	dialogues 	= obj_stories.read_typetree()
 
 	# call the main patcher
-	# patcher(characters['DataList'], skills['DataList'])
+	patcher(characters['DataList'], skills['DataList'], lskills['DataList'])
 	story_patcher(dialogues['DataList'])
 
-	# obj_char.save_typetree(characters)
-	# obj_skills.save_typetree(skills)
+	obj_char.save_typetree(characters)
+	obj_skills.save_typetree(skills)
+	obj_lskills.save_typetree(lskills)
 	obj_stories.save_typetree(dialogues)
 	save()
 
